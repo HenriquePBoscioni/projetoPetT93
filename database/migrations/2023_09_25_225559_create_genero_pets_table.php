d92b3262d96e3cfdb69b285b5200979db0993c32
HenriquePBoscioni <henriquepra10@gmail.com>
2023-09-25 21:45:46 -0300

    migrations e controllers mexidas

--- database/migrations/2023_09_25_225559_create_genero_pets_table.php
+++ database/migrations/2023_09_25_225559_create_genero_pets_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\GeneroPets;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,10 +12,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('genero_pets', function (Blueprint $table) {
-            $table->id();
+        Schema::create('generoPets', function (Blueprint $table) {
+            $table->increments('id_genero');
+            $table->string('genero',45);
             $table->timestamps();
+            $table->softDeletes();
         });
+
+        \App\Models\GeneroPets::create([
+            'id_genero' => 1,
+            'genero' => 'masculino'
+
+        ]);
+        \App\Models\GeneroPets::create([
+            'id_genero' => 2,
+            'genero' => 'feminino'
+
+        ]);
+
+
     }
 
     /**
@@ -22,6 +38,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('genero_pets');
+        Schema::dropIfExists('generoPets');
     }
 };
