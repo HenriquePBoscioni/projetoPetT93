b017f6025048dee3379baa8f882d12b6206be24d
HenriquePBoscioni <henriquepra10@gmail.com>
2023-10-05 20:17:11 -0300

    inicio da form de adocao

--- resources/views/adocoes/form.blade.php
+++ resources/views/adocoes/form.blade.php
@@ -1,13 +1,115 @@
 @extends('layouts.base')
 @section('content')
-<h1>
-    @if ($adocao)
-        Editando a adocão
-        {{$adocao->ad}}
-        {{-- Começamos a editar a form --}}
-        {{-- teste commit --}}
-    @else
+    <h1 class="mx-3 my-4">
+        <i class="bi bi-wallet2"></i>
+        @if ($adocao)
+            Editar adocao:
+            Nº {{ $adocao->id_adocao }}
+        @else
+            Novo adocao
+        @endif
+    </h1>
+    <form action="{{ $adocao ? route('adocoes.update', ['id' => $adocao->id_adocao]) : route('adocoes.store') }}"
+        method="post" enctype="multipart/form-data" class="row g-3">
+        @csrf
 
-    @endif
-</h1>
 
+
+
+        <div class="col-md-6">
+            <label for="id_cliente" class="form-label">Clientes*</label>
+            <select id="id_cliente" class="form-select" required>
+                <option>Clientes...</option>
+            </select>
+        </div>
+        <div class="col-md-6">
+            <label for="id_cliente" class="form-label">Pets*</label>
+            <select id="id_cliente" class="form-select" required>
+                <option>Pets...</option>
+            </select>
+        </div>
+
+        <div class="col-md-4">
+            <label for="id_cliente" class="form-label">Status*</label>
+            <select id="id_cliente" class="form-select" required>
+                <option>Escolha...</option>
+                {{-- @foreach ($clientes::orderBy('cliente')->get() as $cliente)
+            <option value="{{ $cliente->id_cliente}}"
+            @selected(
+                (
+                    $adocao &&
+                    $adocao->id_cliente == $cliente->id_cliente
+                )
+                ||
+                old('id_cliente') == $cliente->id_cliente
+                )
+                >
+                {{ $cliente->cliente }}
+            </option>
+            @endforeach --}}
+            </select>
+        </div>
+
+        <div class="col-md-4">
+            <label class="form-label" for="dt_inicial">
+                Data da adocao*
+            </label>
+            <input class="form-control" type="date" name="dt_inicial" id="dt_inicial">
+        </div>
+
+        <div class="col-md-4">
+            <label class="form-label" for="dt_inicial">
+                Data da devolucao do pet*
+            </label>
+            <input class="form-control" type="date" name="dt_inicial" id="dt_inicial">
+        </div>
+
+
+        {{-- <div class="col-md-3"> --}}
+        {{-- <label for="id_pet" class="form-label">Pet*</label> --}}
+        {{-- <select id="id_pet" class="form-select" required> --}}
+        {{-- <option value="">Escolha...</option> --}}
+        {{-- @foreach ($pet::orderBy('pet')->get() as $centro)
+                    <option value="{{$centro->id_pet}}"
+                        @selected(
+                            (
+                                $adocao &&
+                                $adocao->id_pet == $centro->id_pet
+                            )
+                            ||
+                            old('id_pet') == $centro->id_pet
+                        )
+                    >
+                        {{ $centro->pet}}
+                    </option>
+                @endforeach --}}
+        {{-- </select> --}}
+        {{-- </div> --}}
+
+        {{-- <div class="col-md-4">
+            <label class="form-label" for="valor">Valor*</label>
+            <input class="form-control" type="number" id="valor" name="valor"
+                value="{{ $adocao ? $adocao->valor : old('valor') }}" required>
+        </div> --}}
+
+
+        <div class="col-md-12">
+            <label class="form-label" for="descricao">Observaçoes*</label>
+            <input class="form-control" type="text" id="descricao" name="descricao"
+                value="{{ $adocao ? $adocao->descricao : old('descricao') }}" required>
+        </div>
+        <div class="col-md-12">
+            <label class="form-label" for="descricao">Historico da Adocao*</label>
+            <input class="form-control" type="text" id="descricao" name="descricao"
+                value="{{ $adocao ? $adocao->descricao : old('descricao') }}" required>
+        </div>
+        <div class="col-md-2 offset-md-11">
+            <input class="btn btn-primary" type="submit" value="{{ $adocao ? 'Atualizar' : 'Cadastrar' }}">
+        </div>
+
+
+    </form>
+@endsection
+@section('scripts')
+    @parent
+@endsection
