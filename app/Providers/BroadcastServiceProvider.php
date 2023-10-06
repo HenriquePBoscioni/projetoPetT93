cb306b2dc81bd804693333016da739724e68ab9e
Rafael Jesus <rafael.jesus17@hotmail.com>
2023-10-05 21:50:19 -0300

    1

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Providers;
-
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class BroadcastServiceProvider extends ServiceProvider
 {
@@ -15,5 +15,7 @@ class BroadcastServiceProvider extends ServiceProvider
         Broadcast::routes();
 
         require base_path('routes/channels.php');
+        Paginator::useBootstrapFive();
+        Paginator::useBootstrapFour();
     }
 }
