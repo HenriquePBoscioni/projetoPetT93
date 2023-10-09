ff4d5276c286eacda92c9a64c0442e5b718d6e56
Gustavo Vieira Sakimoto <gustavosakimoto300@gmail.com>
2023-10-09 19:41:44 -0300

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
 
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
