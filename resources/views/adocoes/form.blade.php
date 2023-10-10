4fdef41c42c545bec1f001c735f51bbe963967c0
HenriquePBoscioni <henriquepra10@gmail.com>
2023-10-10 19:26:50 -0300

    .

--- resources/views/adocoes/form.blade.php
+++ resources/views/adocoes/form.blade.php
@@ -18,43 +18,37 @@
 
         <div class="col-md-6">
             <label for="id_cliente" class="form-label">Clientes*</label>
-            <select name="id_cliente" id="id_cliente" class="form-select">
+            <select id="id_cliente" class="form-select" required>
                 <option>Clientes...</option>
-                @foreach ($clientes::all() as $cliente)
-                    <option value="{{$cliente->id_cliente}}"
-                    @selected(($adocao && $adocao->id_cliente == $cliente->id_cliente) || old('id_cliente') == $cliente->id_cliente)>
-                    {{ $cliente->cliente}}
-                    </option>
-                @endforeach
             </select>
         </div>
         <div class="col-md-6">
             <label for="id_cliente" class="form-label">Pets*</label>
             <select id="id_cliente" class="form-select" required>
                 <option>Pets...</option>
-                @foreach ($pets::all() as $pet)
-                <option value="{{$pet->id_pet}}"
-                @selected(($adocao && $adocao->id_pet == $pet->id_pet) || old('id_pet') == $pet->id_pet)>
-                {{ $pet->pet}}
-                </option>
-            @endforeach
-
             </select>
         </div>
 
-        <div class="col-md-4">
+        {{-- <div class="col-md-4">
             <label for="id_cliente" class="form-label">Status*</label>
             <select id="id_cliente" class="form-select" required>
                 <option>Escolha...</option>
-                @foreach ($status::all() as $status)
-                <option value="{{$status->id_status}}"
-                @selected(($adocao && $adocao->id_status == $status->id_status) || old('id_status') == $status->id_status)>
-                {{ $status->status}}
-                </option>
+                @foreach ($clientes::orderBy('cliente')->get() as $cliente)
+            <option value="{{ $cliente->id_cliente}}"
+            @selected(
+                (
+                    $adocao &&
+                    $adocao->id_cliente == $cliente->id_cliente
+                )
+                ||
+                old('id_cliente') == $cliente->id_cliente
+                )
+                >
+                {{ $cliente->cliente }}
+            </option>
             @endforeach
-
             </select>
-        </div>
+        </div> --}}
 
         <div class="col-md-4">
             <label class="form-label" for="dt_inicial">
@@ -71,27 +65,32 @@
         </div>
 
 
-        {{-- <div class="col-md-3">
-        <label for="id_pet" class="form-label">Pet*</label> --}}
-        {{-- <select id="id_pet" class="form-select" required> --}}
-        {{-- <option value="">Escolha...</option> --}}
-        {{-- @foreach ($pet::orderBy('pet')->get() as $centro) --}}
-        {{-- <option value="{{$centro->id_pet}}" --}}
-        {{-- @selected( --}}
-        {{-- ( --}}
-        {{-- $adocao && --}}
-        {{-- $adocao->id_pet == $centro->id_pet --}}
-        {{-- ) --}}
-        {{-- || --}}
-        {{-- old('id_pet') == $centro->id_pet --}}
-        {{-- ) --}}
-        {{-- > --}}
-        {{-- {{ $centro->pet}} --}}
-        {{-- </option> --}}
-        {{-- @endforeach --}}
-        {{-- </select> --}}
-        {{-- </div> --}}
+        <div class="col-md-3">
+        <label for="id_pet" class="form-label">Pet*</label>
+        <select id="id_pet" class="form-select" required>
+            <option value="">Escolha...</option>
+            @foreach ($pet::orderBy('pet')->get() as $pet)
+            <option value="{{$pet->id_pet}}"
+                        @selected(
+                            (
+                                $adocao &&
+                                $adocao->id_pet == $pet->id_pet
+                            )
+                            ||
+                            old('id_pet') == $pet->id_pet
+                        )
+                    >
+                        {{ $pet->pet}}
+                    </option>
+                @endforeach
+        </select>
+        </div>
 
+        <div class="col-md-4">
+            <label class="form-label" for="valor">Valor*</label>
+            <input class="form-control" type="number" id="valor" name="valor"
+                value="{{ $adocao ? $adocao->valor : old('valor') }}" required>
+        </div>
 
 
         <div class="col-md-12">
